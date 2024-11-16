f88c3a8ce2e09385b1e5683f2d526f6885e694e3
sagar <sagaryadav12369@gmail.com>
2024-11-16 23:16:11 +0530

    today

--- app/src/main/java/com/example/drappoinmentapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/drappoinmentapp/Activity/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.drappoinmentapp.Activity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.drappoinmentapp.R
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.transition.Visibility
+import com.example.drappoinmentapp.Adapter.NearDoctorAdapter
+import com.example.drappoinmentapp.ViewModel.MainViewModel
+import com.example.drappoinmentapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var viewModel = MainViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNearByDoctor()
+    }
 
+    private fun initNearByDoctor() {
+        binding.apply {
+            progressBar.visibility = View.VISIBLE
+            viewModel.loadDoctors().observe(this@MainActivity, Observer {
+                topView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+                topView.adapter = NearDoctorAdapter(it)
+                progressBar.visibility = View.GONE
+            })
+        }
     }
 }
